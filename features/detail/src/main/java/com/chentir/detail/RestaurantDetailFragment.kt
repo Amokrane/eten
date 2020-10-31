51ed4e68fa1aebf73fb968c6777156959a2ce884
Amokrane Chentir <ma.chentir@criteo.com>
2020-10-31 17:43:04 +0100

    Add maps and detail feature modules

---
+++ features/detail/src/main/java/com/chentir/detail/RestaurantDetailFragment.kt
@@ -0,0 +1,31 @@
+package com.chentir.detail
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class RestaurantDetailFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = RestaurantDetailFragment()
+    }
+
+    private lateinit var viewModel: RestaurantDetailViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.restaurant_detail_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(RestaurantDetailViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
