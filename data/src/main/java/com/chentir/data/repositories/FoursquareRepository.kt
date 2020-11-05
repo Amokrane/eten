c0ab71b4d67cba85f526edef50fdac8cc623462c
Amokrane Chentir <ma.chentir@criteo.com>
2020-11-05 18:19:29 +0100

    Request location permission

--- data/src/main/java/com/chentir/data/repositories/FoursquareRepository.kt
+++ data/src/main/java/com/chentir/data/repositories/FoursquareRepository.kt
@@ -15,7 +15,6 @@ import java.lang.Exception
 class FoursquareRepository(private val searchVenuesService: SearchVenuesService) : SearchRestaurantsRepository {
     override suspend fun searchRestaurants(lat: String, lng: String): Flow<List<Restaurant>> =
         flow {
-            Timber.d("executing flow!!!!!!!!")
             try {
                 val venuesResult =
                     searchVenuesService.searchVenues("$lat,$lng")
--- features/maps/src/main/java/com/chentir/maps/RestaurantMapsFragment.kt
+++ features/maps/src/main/java/com/chentir/maps/RestaurantMapsFragment.kt
@@ -1,20 +1,34 @@
 package com.chentir.maps
 
+import android.Manifest
+import android.content.pm.PackageManager.PERMISSION_DENIED
+import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import com.chentir.domain.entities.Restaurant
-import com.google.android.libraries.maps.GoogleMap
-import com.google.android.libraries.maps.OnMapReadyCallback
-import com.google.android.libraries.maps.SupportMapFragment
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import timber.log.Timber
 
 class RestaurantMapsFragment : Fragment(), OnMapReadyCallback {
     private val viewModel: RestaurantMapsViewModel by sharedViewModel()
+
+    private val locationPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                initMap()
+            } else {
+                Timber.d("Permission not granted")
+            }
+        }
 
     companion object {
         fun newInstance() = RestaurantMapsFragment()
@@ -29,18 +43,40 @@ class RestaurantMapsFragment : Fragment(), OnMapReadyCallback {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-
-
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
-        mapFragment?.getMapAsync(this)
-
-        val liveData = viewModel.getNearestRestaurants("48.904474", "2.072183")
-        liveData.observe(viewLifecycleOwner, Observer<List<Restaurant>> {
-                Timber.d("Nearest Restaurants: $it")
-            })
+        requestLocationPermission()
     }
 
     override fun onMapReady(p0: GoogleMap?) {
-        Timber.d("onMapReady called")
+        showNearestRestaurants()
+    }
+
+    private fun requestLocationPermission() {
+        val locationPermissionStatus = ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+
+        when (locationPermissionStatus) {
+            PERMISSION_GRANTED -> initMap()
+            PERMISSION_DENIED -> {
+                if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                    Timber.d("should show request permission rationale")
+                } else {
+                    locationPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+                }
+            }
+        }
+    }
+
+    private fun initMap() {
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+    }
+
+    private fun showNearestRestaurants() {
+        val liveData = viewModel.getNearestRestaurants("48.904474", "2.072183")
+        liveData.observe(viewLifecycleOwner, Observer<List<Restaurant>> {
+            Timber.d("Nearest Restaurants: $it")
+        })
     }
 }
